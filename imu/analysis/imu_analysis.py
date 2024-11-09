8cdc7acc0333de51519178e45e8511efc2f00109
Anushka Pradeep Chauhan <chauhan.anus@northeastern.edu>
2024-11-08 21:11:19 -0500

    commit

--- imu/analysis/imu_analysis.py
+++ imu/analysis/imu_analysis.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-# Load ROS bag file and extract data
 def load_bag_data(bag_path):
     bag = rosbag.Bag(bag_path)
     time = []
@@ -19,8 +18,6 @@ def load_bag_data(bag_path):
         accel_x.append(msg.imu.linear_acceleration.x)
         accel_y.append(msg.imu.linear_acceleration.y)
         accel_z.append(msg.imu.linear_acceleration.z)
-
-        # Convert quaternions to Euler angles
         qx, qy, qz, qw = msg.imu.orientation.x, msg.imu.orientation.y, msg.imu.orientation.z, msg.imu.orientation.w
         roll_val = math.atan2(2.0 * (qw * qx + qy * qz), 1.0 - 2.0 * (qx ** 2 + qy ** 2))
         pitch_val = math.asin(2.0 * (qw * qy - qz * qx))
@@ -32,7 +29,6 @@ def load_bag_data(bag_path):
 
     bag.close()
 
-    # Normalize time to start at 0
     start_time = time[0]
     time = [t - start_time for t in time]
 
@@ -43,7 +39,6 @@ def load_bag_data(bag_path):
         'roll': roll, 'pitch': pitch, 'yaw': yaw
     })
 
-# Updated plotting function to handle numpy array conversion
 def plot_time_series(data, column_names, ylabel, title, fig_num):
     plt.figure(fig_num, figsize=(10, 6))
     for i, column in enumerate(column_names, 1):
@@ -66,10 +61,8 @@ def plot_histograms(data, column_names, ylabel, title, fig_num):
     plt.suptitle(title)
     plt.tight_layout(rect=[0, 0, 1, 0.96])
 
-# Load data from specified ROS bag file path
 data = load_bag_data('/home/chauhan-anu/catkin_ws/src/lab3/data/data.bag')
 
-# Generate required plots
 plot_time_series(data, ['gyro_x', 'gyro_y', 'gyro_z'], 'Degrees/s', 'Fig 0: Gyro Rotational Rate', 0)
 plot_time_series(data, ['accel_x', 'accel_y', 'accel_z'], 'm/s²', 'Fig 1: Accelerometer', 1)
 plot_time_series(data, ['roll', 'pitch', 'yaw'], 'Degrees', 'Fig 2: VN Orientation (Euler Angles)', 2)
